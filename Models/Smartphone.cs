9d6987a617f8eab9df72aae6f434ad1093f29074
Amanda DuKawa <110678035+madukisp@users.noreply.github.com>
2024-01-10 06:15:13 +0000

    Finalizando exercicio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,23 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; private set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
 
-        public Smartphone(string numero)
+        
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+            
 
         public void Ligar()
         {
@@ -24,3 +33,5 @@ namespace DesafioPOO.Models
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
+
+
